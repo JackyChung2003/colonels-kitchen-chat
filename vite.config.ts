cc7f2650da265e0b01b32d990c098246b5d51297
Jacky Chung <clashofjacky@email.com>
2024-10-29 11:11:05 +0800

    Added Vite PWA

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@
 
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -16,7 +17,31 @@ export default defineConfig(({ mode }) => {
     define: {
       'process.env.SOME_KEY': JSON.stringify(env.SOME_KEY)
     },
-    plugins: [react()],
+    plugins: [
+      react(),
+      VitePWA({ 
+        registerType: 'autoUpdate' ,
+        includeAssets: ['favicon.ico', 'robots.txt', 'colonel-banner.png', 'logo.svg'],
+        manifest: {
+          name: 'Colonel’s Kitchen Chat',
+          short_name: 'ColonelChat',
+          description: 'Engage in conversations with the Colonel himself, powered by AI!',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: 'logo-192x192.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: 'logo-512x512.png',
+              sizes: '512x512',
+              type: 'image/png'
+            }
+          ]
+        }
+      })
+    ],
     base: "/colonels-kitchen-chat/",
   }
 })
